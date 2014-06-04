f2a771f3b41db7ae12f45a6323aa0b442f9a39d5
Alexander Marushka <marushkaalex@gmail.com>
2014-06-04 12:18:36 +0600

    megarefactoring

--- src/main/java/com/epam/am/hw/Runner.java
+++ src/main/java/com/epam/am/hw/Runner.java
@@ -3,7 +3,6 @@ package com.epam.am.hw;
 public class Runner {
     public static void main(String[] args) {
         Greater greater = new Greater();
-        greater.setGreeting("HelloWorld");
         System.out.println(greater.getGreeting());
     }
 }
